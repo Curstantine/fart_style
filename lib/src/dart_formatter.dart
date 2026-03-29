83b7715b3d5df0ee01fcfced49935b0c0a458702
Curstantine <Curstantine@proton.me>
2026-03-29 12:20:15 +0530

    fix: change defaultPageWidth to 100

--- lib/src/dart_formatter.dart
+++ lib/src/dart_formatter.dart
@@ -46,7 +46,7 @@ final class DartFormatter {
 
 	/// The page width that the formatter tries to fit code inside if no other
 	/// width is specified.
-	static const defaultPageWidth = 80;
+	static const defaultPageWidth = 100;
 
 	/// The Dart language version that formatted code should be parsed as.
 	///
